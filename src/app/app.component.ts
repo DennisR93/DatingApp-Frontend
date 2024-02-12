0022a8169067de11b47cb7c82b31cf0857f07831
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-12 16:45:08 +0200

    adding http get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating App';
+  users!: any;
+  API_USERS: string = "https://localhost:5001/api/users";
+  constructor(private htttp: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.htttp.get(this.API_USERS).subscribe({
+      next: response => this.users = response,
+      error: err => console.log(err),
+      complete: () => console.log("Finished")
+    })
+  }
 }
