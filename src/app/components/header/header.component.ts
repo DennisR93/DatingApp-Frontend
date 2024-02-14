469375f7b97f9879a2bcae7dd79f38462d3b80b3
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-14 21:04:42 +0200

    implementing another approach

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AccountService} from "../../services/account.service";
+import {Observable, of} from "rxjs";
+import {UserModel} from "../../models/usermodel";
 
 @Component({
   selector: 'app-header',
@@ -8,38 +10,40 @@ import {AccountService} from "../../services/account.service";
 })
 export class HeaderComponent implements OnInit{
 model: any = {};
-loggedIn:boolean = false;
+/*loggedIn:boolean = false;*/
+  // currentUser$ :Observable<UserModel | null> = of(null);
 
-constructor(private accountService: AccountService) {}
+constructor(protected accountService: AccountService) {}
 
   ngOnInit() {
-  this.getCurrentUser();
+/*  this.getCurrentUser();*/
+    // this.currentUser$ = this.accountService.currentUser$;
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
+        // this.loggedIn = true;
       },
       error: error => {
         console.log(error);
-        this.loggedIn = false;
+        // this.loggedIn = false;
       }
     })
   }
 
   logout(){
   this.accountService.logout();
-  this.loggedIn = false;
+  // this.loggedIn = false;
   }
 
-  getCurrentUser(){
+  /*getCurrentUser(){
   this.accountService.currentUser$.subscribe({
     next: user => {
       this.loggedIn = !!user;
     },
     error: error => console.log(error)
   })
-  }
+  }*/
 }
