8f050f4ce65a6e27abf24ceb26dac196e150c251
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-14 20:51:26 +0200

    getting and setting users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {AccountService} from "./services/account.service";
+import {UserModel} from "./models/usermodel";
 
 @Component({
   selector: 'app-root',
@@ -8,16 +10,32 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AppComponent implements OnInit {
   title = 'Dating App';
-  users!: any;
+  users: any;
   API_USERS: string = "https://localhost:5001/api/users";
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private accountService: AccountService) {
   }
 
   ngOnInit() {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+
+    const user:UserModel = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
+
+  getUsers(){
     this.http.get(this.API_USERS).subscribe({
-      next: response => this.users = response,
+      next: response => {
+        this.users = response;
+        console.log(response);
+      },
       error: err => console.log(err),
       complete: () => console.log("Finished")
-    })
+    });
   }
 }
