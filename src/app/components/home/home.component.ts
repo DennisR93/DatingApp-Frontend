35aa04c033a8579c15bbd60aec166611419f367e
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-15 08:57:31 +0200

    adding users to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   registerMode:boolean = false;
+  API_USERS: string = "https://localhost:5001/api/users";
+  users: any;
 
-  constructor() {}
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers(){
+    this.http.get(this.API_USERS).subscribe({
+      next: response => {
+        this.users = response;
+        console.log(response);
+      },
+      error: err => console.log(err),
+      complete: () => console.log("Finished")
+    });
+  }
+
+  cancelRegisterMode(event:boolean){
+    this.registerMode = event;
   }
 }
