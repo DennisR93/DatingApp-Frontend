efbd7784bbbb33e9531d0a48d73d37b9c9ae845d
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-16 18:10:20 +0200

    adding auth gurad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,22 @@ import {MemberListComponent} from "./components/members/member-list/member-list.
 import {MemberDetailComponent} from "./components/members/member-detail/member-detail.component";
 import {ListsComponent} from "./components/lists/lists.component";
 import {MessagesComponent} from "./components/messages/messages.component";
+import {authGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent},
-  {path: 'members/:id', component: MemberDetailComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
-  {path: '**', component: HomeComponent, pathMatch: 'full'}
+  {path: '',
+  runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      {path: 'members', component: MemberListComponent},
+      {path: 'members/:id', component: MemberDetailComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent},
+      {path: '**', component: HomeComponent, pathMatch: 'full'}
+    ]
+  }
+
 ];
 
 @NgModule({
