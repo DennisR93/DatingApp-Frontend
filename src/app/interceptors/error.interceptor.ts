4d8daaa7153d49a1db3b094ef182e6aae3a4a68e
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-18 20:48:13 +0200

    adding error interceptor

---
+++ src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,48 @@
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {catchError, Observable} from "rxjs";
+import {NavigationExtras, Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor{
+  constructor(private router: Router, private toastr: ToastrService) {
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if(error){
+          switch(error.status){
+            case 400:
+              if(error.error.errors){
+                const modelStateErrors = [];
+                for(const key in error.error.errors){
+                  if(error.error.errors[key]){
+                    modelStateErrors.push(error.error.errors);
+                  }
+                }
+                throw modelStateErrors;
+              } else{
+                this.toastr.error(error.error, error.status.toString());
+              }
+              break;
+            case 401:
+              this.toastr.error("Unauthorized", error.status.toString());
+              break;
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500:
+              const navigationExtras: NavigationExtras = {state: {error: error.error}};
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+            default: this.toastr.error("Something unexpected went wrong");
+              console.log(error);
+              break;
+          }
+        }
+        throw error;
+      })
+    )
+  }
+}
