409e4fc3187713afaeded8be8900b437ab6e0bdf
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-20 17:37:35 +0200

    making standalone component

--- src/app/components/members/member-detail/member-detail.component.ts
+++ src/app/components/members/member-detail/member-detail.component.ts
@@ -2,11 +2,19 @@ import {Component, OnInit} from '@angular/core';
 import {MemberModel} from "../../../models/membermodel";
 import {MembersService} from "../../../services/members.service";
 import {ActivatedRoute} from "@angular/router";
+import {DatePipe, NgIf} from "@angular/common";
+import {SharedModule} from "../../../modules/shared.module";
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrl: './member-detail.component.css'
+  styleUrl: './member-detail.component.css',
+  imports: [
+    DatePipe,
+    NgIf,
+    SharedModule
+  ],
+  standalone: true
 })
 export class MemberDetailComponent implements OnInit{
   member: MemberModel | undefined;
