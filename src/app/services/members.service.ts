fbe83948ca3f0f4b35076c88ff3ff0347b5bb0df
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-20 14:32:14 +0200

    creating getMembers and getMember method

--- src/app/services/members.service.ts
+++ src/app/services/members.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {MemberModel} from "../models/membermodel";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
+baseUrl:string = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getMembers(){
+    return this.http.get<MemberModel[]>(this.baseUrl + 'users', this.getHttpOptions())
+  }
+
+  getMember(username: string){
+    return this.http.get<MemberModel>(this.baseUrl + 'users/' + username, this.getHttpOptions());
+  }
+
+  getHttpOptions() {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+
+    const user = JSON.parse(userString);
+
+    return {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer '+ user.token
+      })
+    };
+  }
 }
