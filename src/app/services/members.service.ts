dee50c164aae7d01240dcfaa3c1d54ebd79884e2
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-20 14:45:25 +0200

    creating methods

--- src/app/services/members.service.ts
+++ src/app/services/members.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {environment} from "../../environments/environment";
+import {environment} from "../../environments/environment.development";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {MemberModel} from "../models/membermodel";
 
