c59f3dead0b0c58e2ded4b46f38c10301a6a1ef3
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-20 14:45:13 +0200

    loading members method

--- src/app/components/members/member-list/member-list.component.ts
+++ src/app/components/members/member-list/member-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MemberModel} from "../../../models/membermodel";
+import {MembersService} from "../../../services/members.service";
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit{
+  members: MemberModel[] = [];
+
+  constructor(private memberService: MembersService) { }
+
+  ngOnInit(){
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members
+    })
+  }
 
 }
