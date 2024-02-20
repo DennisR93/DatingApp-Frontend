229e90550f25f02e716e55e9893d81befcc5dd3b
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-20 14:48:00 +0200

    creating member card

---
+++ src/app/components/members/member-card/member-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrl: './member-card.component.css'
+})
+export class MemberCardComponent {
+
+}
