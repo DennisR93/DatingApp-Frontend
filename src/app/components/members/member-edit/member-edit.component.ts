201482ecbce194957acc4bd63546ebeb9fb5effb
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-20 19:53:03 +0200

    adding loadMember method

--- src/app/components/members/member-edit/member-edit.component.ts
+++ src/app/components/members/member-edit/member-edit.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MemberModel} from "../../../models/membermodel";
+import {UserModel} from "../../../models/usermodel";
+import {AccountService} from "../../../services/account.service";
+import {MembersService} from "../../../services/members.service";
+import {take} from "rxjs";
 
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css'
 })
-export class MemberEditComponent {
+export class MemberEditComponent implements OnInit{
+  member : MemberModel | undefined;
+  user: UserModel | null = null;
+  constructor(private accountService: AccountService, private memberService: MembersService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => this.user = user
+    })
+  }
+
+  ngOnInit() {
+    this.loadMember();
+  }
+
+  loadMember(){
+    if(!this.user) return;
+    this.memberService.getMember(this.user.username).subscribe({
+      next: member => this.member = member
+    })
+  }
 
 }
