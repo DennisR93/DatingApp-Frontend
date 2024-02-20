77b9eb44934c4dab3aee5af3e079a2b134124435
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-20 16:56:05 +0200

    loading selected member

--- src/app/components/members/member-detail/member-detail.component.ts
+++ src/app/components/members/member-detail/member-detail.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MemberModel} from "../../../models/membermodel";
+import {MembersService} from "../../../services/members.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit{
+  member: MemberModel | undefined;
 
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.loadMember();
+  }
+
+  loadMember(){
+    const username = this.route.snapshot.paramMap.get('username');
+    if(!username) return;
+
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    });
+  }
 }
