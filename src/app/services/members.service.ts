78cba1a7e705eaf5869ea08e3ab36c7ecc0586d6
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-21 09:27:38 +0200

    adding state to users

--- src/app/services/members.service.ts
+++ src/app/services/members.service.ts
@@ -2,19 +2,29 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment.development";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {MemberModel} from "../models/membermodel";
+import {map, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
 baseUrl:string = environment.apiUrl;
+members: MemberModel[] = [];
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<MemberModel[]>(this.baseUrl + 'users')
+    if(this.members.length > 0) return of(this.members);
+    return this.http.get<MemberModel[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string){
+    const member= this.members.find(u => u.userName === username);
+    if(member) return of(member);
     return this.http.get<MemberModel>(this.baseUrl + 'users/' + username);
   }
 
