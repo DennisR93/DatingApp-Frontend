1de9c117c8d9bdeda19a56f4e00daf148042047e
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-21 09:01:45 +0200

    adding spinner

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 import {ToastrModule} from "ngx-toastr";
 import {TabsModule} from "ngx-bootstrap/tabs";
+import {NgxSpinnerModule} from "ngx-spinner";
 
 
 
@@ -12,12 +13,14 @@ import {TabsModule} from "ngx-bootstrap/tabs";
     CommonModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
-    ToastrModule.forRoot({positionClass: 'toast-center-center'})
+    ToastrModule.forRoot({positionClass: 'toast-center-center'}),
+    NgxSpinnerModule.forRoot({type: 'triangle-skew-spin'})
   ],
   exports:[
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
