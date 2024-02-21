ba0b13f8888d01b82ab804f0477791d5ab569d87
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-21 16:52:03 +0200

    validating matching values in password and confirmPassword

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AccountService} from "../../services/account.service";
 import {ToastrService} from "ngx-toastr";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-form',
@@ -23,8 +23,17 @@ export class RegisterFormComponent implements OnInit{
     this.registerForm = new FormGroup({
       username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(8)]),
       password: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
-      confirmPassword: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
+      confirmPassword: new FormControl("", [Validators.required, this.matchValues('password')]),
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     })
+  }
+
+  matchValues(matchTo: string): ValidatorFn{
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register(){
