44c907caf5018316be23ad3296376225b4b50845
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-23 20:45:01 +0200

    adding add like method

--- src/app/components/members/member-card/member-card.component.ts
+++ src/app/components/members/member-card/member-card.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {MemberModel} from "../../../models/membermodel";
+import {MembersService} from "../../../services/members.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-member-card',
@@ -9,4 +11,11 @@ import {MemberModel} from "../../../models/membermodel";
 export class MemberCardComponent {
   @Input() member: MemberModel | undefined;
 
+  constructor(private memberService: MembersService, private toastr: ToastrService) {}
+
+  addLike(member: MemberModel){
+    this.memberService.addLike(member.userName).subscribe({
+      next: () => this.toastr.success("You have liked " + member.knownAs)
+    })
+  }
 }
