80be998b76804c3d74e74b36cc1d762e9dc1e0ba
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-23 20:45:38 +0200

    creating load likes method

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MemberModel} from "../../models/membermodel";
+import {MembersService} from "../../services/members.service";
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
+  members: MemberModel[] = [];
+  predicate:string = 'liked';
 
+  constructor(private memberService: MembersService) {
+  }
+
+  ngOnInit() {
+    this.loadLikes();
+  }
+
+  loadLikes(){
+    this.memberService.getLikes(this.predicate).subscribe({
+      next: response => {
+        this.members = response
+      }
+    })
+  }
 }
