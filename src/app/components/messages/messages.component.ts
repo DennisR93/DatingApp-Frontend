7acf15a82398fdd3eae76df41d0f8197d8ebb27c
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-24 10:22:49 +0200

    creating messages component

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Message} from "../../models/message";
+import {Pagination} from "../../models/pagination";
+import {MessageService} from "../../services/message.service";
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messages?: Message[];
+  pagination?: Pagination;
+  container:string = "Unread";
+  pageNumber:number = 1;
+  pageSize:number = 5;
 
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(){
+    this.loadMessages();
+  }
+
+  loadMessages(){
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
+      next: response => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+      }
+    })
+  }
+
+  pageChanged(event: any){
+    if(this.pageNumber !== event.page){
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
