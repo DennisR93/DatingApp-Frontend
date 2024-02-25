d7d46db6f84ee856a8656081c49fb256b2785e2a
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-25 18:05:18 +0200

    adding method to component

--- src/app/components/admin-panel/user-management/user-management.component.ts
+++ src/app/components/admin-panel/user-management/user-management.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AdminService} from "../../../services/admin.service";
+import {UserModel} from "../../../models/usermodel";
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.css'
 })
-export class UserManagementComponent {
+export class UserManagementComponent implements OnInit{
+  users: UserModel[] = [];
 
+  constructor(private adminService: AdminService) {
+  }
+
+  ngOnInit() {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles(){
+    this.adminService.getUsersWithRoles().subscribe({
+      next: users => this.users = users
+    })
+  }
 }
