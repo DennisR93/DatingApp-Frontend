8e1b934497497fa8b1166f14c92686ad35e7354b
dennisrabinovitch <rdennis0993@gmail.com>
2024-02-26 11:17:40 +0200

    adding messageService

--- src/app/components/members/member-messages/member-messages.component.ts
+++ src/app/components/members/member-messages/member-messages.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from "../../../models/message";
-import {NgForOf, NgIf} from "@angular/common";
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {TimeagoModule} from "ngx-timeago";
 import {MessageService} from "../../../services/message.service";
 import {FormsModule, NgForm} from "@angular/forms";
@@ -13,25 +13,25 @@ import {FormsModule, NgForm} from "@angular/forms";
     TimeagoModule,
     NgForOf,
     NgIf,
-    FormsModule
+    FormsModule,
+    AsyncPipe
   ],
   standalone: true
 })
 export class MemberMessagesComponent{
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
-  @Input() messages: Message[] = [];
   messageContent = '';
 
-  constructor(private messageService: MessageService) {}
+  constructor(protected messageService: MessageService) {}
 
   sendMessage(){
     if(!this.username) return;
 
     this.messageService.sendMessage(this.username, this.messageContent).subscribe({
       next: message => {
-        this.messages.push(message);
-        this.messageForm?.reset();
+        // this.messages.push(message);
+        // this.messageForm?.reset();
       }
     })
   }
